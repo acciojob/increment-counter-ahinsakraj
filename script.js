137544a6e640a8ef8a5cb1a1dc1de5dc53099721
Ahinsak Raj <117904029+ahinsakraj@users.noreply.github.com>
2023-12-21 15:36:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let cnt=0;
+const btn=document.getElementById('incrementBtn');
+btn.addEventListener('click',()=>{
+	cnt++;
+	const text=document.getElementById('counter');
+	text.innerText=cnt;
+})
